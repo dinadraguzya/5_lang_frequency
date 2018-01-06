ebf345f26bedd3ce78d3f94232b5394c3eaa1495
Dina <dina.draguzya@gmail.com>
2018-01-07 01:37:14 +0200

    Added functions to analyse words frequency

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import re
+import collections
+import os
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r') as opened_file:
+        return opened_file.read().lower()
 
 
 def get_most_frequent_words(text):
-    pass
+    words_list = re.sub('[\W]', ' ', text).split()
+    counter = collections.Counter(words_list)
+    return counter.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    file_path = sys.argv[1]
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        text = load_data(file_path)
+        words = get_most_frequent_words(text)
+        print('The top 10 most frequent words:\n')
+        for word in words:
+            print('{} : {}'.format(word[0], word[1]))
+    else:
+        print("The file doesn't exist!")
