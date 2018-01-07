66db2155817948e737e3908deb9ab824b4fb481b
Dina <dina.draguzya@gmail.com>
2018-01-07 10:29:50 +0200

    Refactored code

--- lang_frequency.py
+++ lang_frequency.py
@@ -12,7 +12,8 @@ def load_data(file_path):
 def get_most_frequent_words(text):
     words_list = re.sub('[\W]', ' ', text).split()
     counter = collections.Counter(words_list)
-    return counter.most_common(10)
+    words_count = 10
+    return counter.most_common(words_count)
 
 
 if __name__ == '__main__':
