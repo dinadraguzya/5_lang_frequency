cd6c94d8a0275146caf7a9e90114b703f61f7849
Dina <dina.draguzya@gmail.com>
2018-01-08 21:53:59 +0200

    Refactored code

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,7 +13,7 @@ def get_most_frequent_words(text):
     words_list = re.sub('[\W]', ' ', text).lower().split()
     counter = collections.Counter(words_list)
     words_count = 10
-    return dict(counter.most_common(words_count))
+    return counter.most_common(words_count)
 
 
 if __name__ == '__main__':
@@ -24,9 +24,9 @@ if __name__ == '__main__':
     else:
         if os.path.exists(file_path) and os.path.isfile(file_path):
             text = load_data(file_path)
-            words_dict = get_most_frequent_words(text)
+            words_list = get_most_frequent_words(text)
             print('The top 10 most frequent words:\n')
-            for word in words_dict:
-                print('{} : {}'.format(word, words_dict[word]))
+            for word, count in words_list:
+                print('{} : {}'.format(word, count))
         else:
             print("The file doesn't exist!")
